6de8fd2ed1e62730306104c07fec738ded677842
SaadMu7ammad <74321306+SaadMu7ammad@users.noreply.github.com>
2023-03-27 02:53:17 +0200

    version without local storage feature

--- 2-movie-seat-booking/script.js
+++ 2-movie-seat-booking/script.js
@@ -2,42 +2,19 @@ const seatsWithScreen = document.querySelector('.seatsWithScreen');
 const changeOnUiPrice = document.querySelector('#total');
 const changeOnUi = document.querySelector('#count');
 const selectedMovie = document.getElementById('movie'); //the price of each movie in the select
-const seats = document.querySelectorAll('.col .seat:not(.occupied');//all seats not occupied
-showUi();
-//get data from local storage and show it
-function showUi(){
-  const selectedSeats =JSON.parse(localStorage.getItem('selectedSeatsArr'));
-console.log(selectedSeats);
-}
-//save selected movie indx and price
-function setMovieData(indx,val) {
-  localStorage.setItem('selectedMoviendex',JSON.stringify(indx));
-  localStorage.setItem('Price',JSON.stringify(val));
 
-}
 function countSelectedSeats() {
-  const selectedSeats = seatsWithScreen.querySelectorAll('.col .seat.selected');
+  const c = seatsWithScreen.querySelectorAll('.seat.selected');
   //   console.log(c.length);
-  // console.log(selectedSeats);
-  changeOnUi.textContent = selectedSeats.length;
-  //   selectedSeatsonsole.log(changeOnUi);
-  changeOnUiPrice.innerText =
-    selectedSeats.length * parseInt(selectedMovie.value);
+  changeOnUi.textContent = c.length;
+  //   console.log(changeOnUi);
+  changeOnUiPrice.innerText = c.length * parseInt(selectedMovie.value);
   // console.log(+selectedMovie.value);
-  const seatsIndx = [...selectedSeats].map((seat) => {
-    // console.log(seat);
-    // console.log( [...seats].indexOf(seat));
-    return [...seats].indexOf(seat);
-  });
-  console.log(seatsIndx);
-  localStorage.setItem('selectedSeatsArr',JSON.stringify(seatsIndx));
 }
 
 //movie select update
-selectedMovie.addEventListener('change', (e) => {
-  countSelectedSeats(e.target.s);
-  console.log(e.target.selectedIndex,e.target.value);
- setMovieData(e.target.selectedIndex,+e.target.value);
+selectedMovie.addEventListener('change', () => {
+  countSelectedSeats();
 });
 
 //seat event click
@@ -47,4 +24,3 @@ seatsWithScreen.addEventListener('click', (ev) => {
     countSelectedSeats();
   }
 });
-
